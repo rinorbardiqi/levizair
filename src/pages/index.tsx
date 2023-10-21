0fb440d3fb0c8bd778c3cb125366345a0b402da7
Rinor Bardiqi <rinor@pabau.com>
2023-10-22 00:33:05 +0200

    WIP hero section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,32 @@
-import { signOut, useSession } from "next-auth/react";
 import Head from "next/head";
-import { Button } from "antd";
+import { Layout } from "antd";
+import heroImage from "public/images/hero.png";
+import scroll from "../lottie/scroll.json";
+import Image from "next/image";
+import Lottie from "lottie-react";
 
-import { api } from "@api";
+const { Header } = Layout;
+
+const menu = [
+  {
+    key: "1",
+    name: "Deals for you",
+    active: true,
+  },
+  {
+    active: false,
+    key: "2",
+    name: "Upcoming Events",
+  },
+  {
+    key: "3",
+    name: "My profile",
+    active: false,
+    icon: "images/user.svg",
+  },
+];
 
 export default function Home() {
-  const hello = api.post.hello.useQuery({ text: "from tRPC" });
-
-  const { data: session, status } = useSession();
   return (
     <>
       <Head>
@@ -15,41 +34,47 @@ export default function Home() {
         <meta name="description" content="levizair " />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-slate-50">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h4 className="text-5xl font-extrabold   ">
-            you are currently {status}
-            {session ? session.user.name : ""}
-          </h4>
-
-          {session?.user ? (
-            <Button onClick={() => void signOut()} className="">
-              Log out
-            </Button>
-          ) : (
-            <>
-              <Button
-                href="/login"
-                className=" h-30 flex w-80 justify-center self-center  p-2 pb-3 text-yellow-50"
-              >
-                Log in
-              </Button>
-
-              <Button
-                href="/register"
-                className=" h-30 flex w-80 justify-center self-center  p-2 pb-3 text-yellow-50"
-              >
-                Register
-              </Button>
-            </>
-          )}
-
-          <div className="flex flex-col items-center gap-2">
-            <p className="text-2xl ">
-              {hello.data != null
-                ? hello.data.greeting
-                : "Loading tRPC query..."}
-            </p>
+      <main>
+        <Header
+          style={{ display: "flex", alignItems: "center" }}
+          className="h-20 bg-fuchsia-50 px-16"
+        >
+          <Image src="/images/logo.svg" alt="logo" width={215} height={53} />
+          <div className="relative ml-auto flex">
+            {menu.map((item) => (
+              <span className="ml-10 flex text-base" key={item.key}>
+                {item.icon && (
+                  <Image
+                    src="/images/user.svg"
+                    alt="logo"
+                    width={20}
+                    height={20}
+                  />
+                )}
+                {item.name}
+              </span>
+            ))}
+          </div>
+        </Header>
+        <div
+          className="relative bg-auto bg-center bg-no-repeat"
+          style={{ height: "80vh" }}
+        >
+          <Image
+            src={heroImage}
+            alt="hero"
+            className="h-full w-full bg-cover bg-center object-cover"
+          />
+          <div className="absolute left-24 top-1/4">
+            <div className=" mb-6 max-w-[450px] text-6xl font-medium uppercase leading-[70px]  text-white">
+              A cristal clear airline experience
+            </div>
+            <div className="max-w-[320px] text-lg font-normal leading-7 text-white">
+              We believe in invisible details that make the difference.
+            </div>
+            <div className="-ml-[180px] max-h-28 max-w-sm">
+              <Lottie animationData={scroll} width={100} />
+            </div>
           </div>
         </div>
       </main>
