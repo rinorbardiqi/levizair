380a65464507a2e794bb678598cbb5183d0f16e9
Rinor Bardiqi <rinor@pabau.com>
2023-10-22 05:07:26 +0200

    finish layout of front page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,16 @@ import heroImage from "public/images/hero.png";
 import scroll from "../lottie/scroll.json";
 import Image from "next/image";
 import Lottie from "lottie-react";
+import suggestImg from "public/images/suggestPlace.png";
+import planeImg from "public/images/plane.png";
+import referedGif from "public/images/referedGif.gif";
+import footerImg from "public/images/Footer.png";
 import HeaderLayout from "~/components/layout/Header";
-import { Row, Col, Card } from "antd";
+import { Row, Col, Card, Button, Modal } from "antd";
+import { useState } from "react";
 
 export default function Home() {
+  const [openReferedBy, setOpenReferedBy] = useState(false);
   return (
     <>
       <Head>
@@ -100,6 +106,101 @@ export default function Home() {
             />
           </div>
         </div>
+        <div className="my-28">
+          <Image src={suggestImg} alt="" className="w-full" />
+        </div>
+        <div className="mx-20 mb-28 flex">
+          <div className="mr-5 h-[620px] w-2/5 rounded-2xl bg-white p-8 ">
+            <p className="mb-10 font-neue text-4xl font-medium uppercase">
+              <span className="text-mblue">Refer Friends</span>, Earn Together,
+              Travel Forever
+            </p>
+            <Image
+              src={planeImg}
+              alt=""
+              className="relative -m-8 mb-4 ml-auto"
+            />
+            <p className="mb-10 font-montserrat text-lg">
+              Refer friends, earn exclusive travel rewards, and make lifelong
+              travel memories together!
+            </p>
+            <Button
+              className="h-14 w-full bg-mblue text-white"
+              onClick={() => setOpenReferedBy(true)}
+            >
+              <span className="!flex !flex-row items-center justify-center">
+                <Image
+                  src="images/referUser.svg"
+                  alt=""
+                  width={20}
+                  className="m-2"
+                  height={20}
+                />
+                Refer Friends
+              </span>
+            </Button>
+          </div>
+          <div>
+            <Image
+              src={referedGif}
+              alt=""
+              className="h-[620px] w-full rounded-2xl"
+            />
+          </div>
+        </div>
+        <div className="relative">
+          <Image src={footerImg} alt="" className="h-72 w-full" />
+          <Image
+            src="images/whiteLogo.svg"
+            alt=""
+            width={150}
+            height={50}
+            className="absolute left-[12%] top-[20%]"
+          />
+          <p className="absolute bottom-[20%] left-[12%] font-montserrat text-base text-mwhite">
+            2023 © LevizAir. All rights reserved.
+          </p>
+          <p className="absolute bottom-[20%] right-[12%] font-montserrat text-base text-mwhite">
+            General terms & conditions of carriage / Terms of Use / Privacy
+            Policy / Cookies / Contact us / Cookie preferences
+          </p>
+        </div>
+        <Modal
+          open={openReferedBy}
+          onCancel={() => setOpenReferedBy(false)}
+          destroyOnClose
+          centered
+          width={600}
+          footer={null}
+          className="flex  flex-col items-center justify-center"
+        >
+          <div className="!flex w-full !flex-col !items-center !justify-center p-7">
+            <Image
+              src="images/modalRefered.svg"
+              alt=""
+              width={190}
+              height={130}
+            />
+            <p className="my-3  max-w-xs text-center font-montserrat">
+              Earn Loyalty Points for every new friend that uses our services!
+            </p>
+            <Button
+              className="h-14 w-full bg-mblue text-white"
+              onClick={() => setOpenReferedBy(false)}
+            >
+              <span className="!flex !flex-row items-center justify-center">
+                <Image
+                  src="images/link.svg"
+                  alt=""
+                  width={20}
+                  className="m-2"
+                  height={20}
+                />
+                Copy link
+              </span>
+            </Button>
+          </div>
+        </Modal>
       </main>
     </>
   );
