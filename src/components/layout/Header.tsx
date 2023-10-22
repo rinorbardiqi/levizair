9f6e559b0235a834978f3727061eb6eb36c70900
Rinor Bardiqi <rinor@pabau.com>
2023-10-22 02:46:41 +0200

    add layout wip

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -22,34 +22,42 @@ const menu = [
 
 const HeaderLayout = () => {
   return (
-    <Header
-      style={{
-        display: "flex",
-        alignItems: "center",
-        position: "fixed",
-        top: 0,
-        zIndex: 1,
-        width: "100%",
-      }}
-      className="h-20 bg-white px-16"
-    >
-      <Image src="/images/logo.svg" alt="logo" width={215} height={53} />
-      <div className="relative ml-auto flex">
-        {menu.map((item) => (
-          <span
-            className={`ml-10 flex font-montserrat text-base leading-6 ${
-              item.active ? "text-[#3957CC]" : "text-black"
-            }`}
-            key={item.key}
-          >
-            {item.icon && (
-              <Image src="/images/user.svg" alt="logo" width={20} height={20} />
-            )}
-            {item.name}
-          </span>
-        ))}
-      </div>
-    </Header>
+    <>
+      <Header
+        style={{
+          display: "flex",
+          alignItems: "center",
+          position: "fixed",
+          top: 0,
+          zIndex: 1,
+          width: "100%",
+        }}
+        className="h-20 bg-white px-16"
+      >
+        <Image src="/images/logo.svg" alt="logo" width={215} height={53} />
+        <div className="relative ml-auto flex">
+          {menu.map((item) => (
+            <span
+              className={`ml-10 flex font-montserrat text-base leading-6 ${
+                item.active ? "text-[#3957CC]" : "text-black"
+              }`}
+              key={item.key}
+            >
+              {item.icon && (
+                <Image
+                  src="/images/user.svg"
+                  alt="logo"
+                  width={20}
+                  height={20}
+                />
+              )}
+              {item.name}
+            </span>
+          ))}
+        </div>
+      </Header>
+      <div className="h-20" />
+    </>
   );
 };
 export default HeaderLayout;
