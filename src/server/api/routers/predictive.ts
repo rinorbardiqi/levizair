4816c40a21ff784b66e988e7c079df9e0c9aa50b
Altin Thaci <altin.thaqi.520@gmail.com>
2023-10-22 09:19:00 +0200

    fix(g-auth): dep issues on g-auth

--- src/server/api/routers/predictive.ts
+++ src/server/api/routers/predictive.ts
@@ -7,7 +7,9 @@ const auth = new google.auth.GoogleAuth({
   credentials: {
     client_id: process.env.GOOGLE_CLIENT_ID,
     client_email: process.env.GOOGLE_CLIENT_EMAIL,
-    private_key: process.env.GOOGLE_PRIVATE_KEY,
+    private_key: process.env.GOOGLE_PRIVATE_KEY?.split(String.raw`\n`).join(
+      "\n",
+    ),
   },
 });
 
